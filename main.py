1158d9e3ea0533590faa83275eff6c389e1a6fde
SABIK Yanis <63198599+Luzog78@users.noreply.github.com>
2022-06-03 18:23:28 +0000

    Demo + .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from turtle import speed, pu, pd, goto, circle as c, dot, write, \
+from turtle import speed, pu, pd, goto, circle as c, dot, write, clear, \
                    hideturtle, exitonclick, tracer, update, setup
 from math import pi, cos, sin
+from time import sleep
 
 setup(width=1.0, height=1.0, startx=0, starty=0)
+hideturtle()
 speed(0)
 
 
@@ -47,15 +49,53 @@ def links(table: float, modulo: int, radius: int):
   pd()
 
 
-table, modulo, radius = 2, 100, 300  # 37.6, 500, 300
-instant = False
+def draw_all(table: float, modulo: int, radius: int, instant: bool = False,
+             _circle: bool = True, _dots: bool = True, _links: bool = True):
+  """
+  Intresting ones:  <br>
+    -  3,      3    <br>
+    -  3,     24    <br>
+    -  2,     11    <br>
+    - 11,    150    <br>
+    - 13,     60    <br>
+    - 13,    600    <br>
+    - 37.6,  500    <br>
+    - 45,     10    <br>
+    - 45,    400    <br>
+    - 45,    800    <br>
+    - 45,   1200    <br>
+  """
+  if instant:
+    tracer(0, 0)
+  if _circle:
+    circle(radius)
+  if _dots:
+    dots(modulo, radius, True)
+  if _links:
+    links(table, modulo, radius)
+  if instant:
+    update()
 
-if instant:
-  tracer(0, 0)
-circle(radius)
-dots(modulo, radius, True)
-links(table, modulo, radius)
-hideturtle()
-if instant:
-  update()
+
+def demo():
+  inp = input("Démo ? (1 / 2 / 3)  : ")
+  if inp == "1":
+    draw_all(45, 800, 350, True, _dots=False)
+    
+  elif inp == "2":
+    for i in range(10_000):
+      draw_all(i / 20, 10, 300, True)
+      sleep(0.1)
+      clear()
+      
+  elif inp == "3":
+    for i in range(10_000):
+      draw_all(i / 15 + 30, 600, 350, True, _circle=False, _dots=False)
+      sleep(0.01)
+      clear()
+
+  else:
+    demo()
+
+demo()
 exitonclick()
